0fa1f88470b24394960d1921fa91e15926a3d90f
core1011 <875590079@qq.com>
2020-12-07 15:51:08 +0800

    add new function

--- updatetask.cpp
+++ updatetask.cpp
@@ -9,8 +9,8 @@ updatetask::updatetask()
 void updatetask::run()
 {
 
-    //read_file();
-    test_frame();
+    read_file();
+    //test_frame();
 
     int size = frame_list.size();
     int len = 0;
@@ -42,7 +42,7 @@ void updatetask::read_file()
     int n=0,sum=0;
     char buf[MAXLEN];
     //char *file0 = "/ll/tps4";
-    char *file0 = "/ll/tps4.tar.gz";
+    char *file0 = "/ll/tps4.zip";
     char *file1 = "/ll/tps4.tar.gz.tmp";
 
     fp = fopen(file0,"r+");
@@ -140,17 +140,30 @@ void updatetask::test_frame()
     char buf[1024]={0};
     int len;
 
-    len = sprintf(buf,"{\"type\":\"softVer\"}");
-    send_frame(COM_ETH,0x80,CMD_READ,len,(unsigned char *)buf);
+    len = sprintf(buf,"{\"meta\":{\"type\":\"softVer\"}}");
+    //send_frame(COM_ETH,0Xd5,0x88,len,(unsigned char *)buf);
 
 
-    len = sprintf(buf,"{\"meta\":{\"type\":\"mq.realTime\"},\"data\":{\"orderCode\":\"A010203040506070809\",\"customerCode\":\"B010203040506070809\",\"customerName\":\"沙雕\",\"styleCode\":\"C010203040506070809\",\"styleName\":\"沙雕2号\",\"colorCode\":\"red\",\"colorName\":\"红色\",\"sizeCode\":\"xxl\",\"sizeName\":\"大码\",\"orderNum\":\"1000\",\"requireTime\":\"20190707\",\"picture\":\"http://img.netbian.com/file/2019/0531/3efc3ea464ded90461ccec12c8d7c677.jpg\",\"planNum\":\"200\",\"hangerCode\":\"11913194\",\"hgType\":\"缺陷衣架\",\"passedStation\":[\"1101-01\",\"1101-02\"],\"paInfo\":[{\"seqNo\":\"01\",\"seqName\":\"裁剪袖口1\",\"standardTime\":\"56\",\"descInfo\":\"左右都有缝制\"},{\"seqNo\":\"02\",\"seqName\":\"裁剪袖口2\",\"standardTime\":\"53\",\"descInfo\":\"左右都有缝制\"},{\"seqNo\":\"03\",\"seqName\":\"裁剪袖口3\",\"standardTime\":\"54\",\"descInfo\":\"左右都有缝制\"},{\"seqNo\":\"02\",\"seqName\":\"裁剪袖口2\",\"standardTime\":\"53\",\"descInfo\":\"左右都有缝制\"}]}}");
-          //  send_frame(COM_ETH,0x80,CMD_READ,len,(unsigned char *)buf);
+    len = sprintf(buf,"{\"data\":{\"colorName\":\"蓝\",\"paInfo\":[{\"seqName\":\"三号\"}],\"customerCode\":\"ceshi1117\",\"orderNum\":\"30\",\"styleName\":\"ceshi1117\",\"hangerCode\":\"3919345\",\"styleCode\":\"ceshi1117\",\"customerName\":\"ceshi1117\",\"requireTime\":\"2020-11-23\",\"planNum\":\"30\",\"hgType\":\"返修衣架\",\"sizeName\":\"L\",\"orderCode\":\"201117-0001\",\"colorCode\":\"03\",\"sizeCode\":\"01\"},\"meta\":{\"type\":\"mq.realTime\"}}");
+            //send_frame(COM_ETH,0x80,CMD_READ,len,(unsigned char *)buf);
             usleep(1000*200);
+
+       //{"data":{"faultNumber":0,"planNum":30,"lastCostTime":"16","hgFinishNumber":2,"totalTime":18,"hangerCode":"3881768","totalStdAvgTime":0,"nextStation":"1101-03","finishNumber":2},"meta":{"type":"mq.lastCostTime"}}
+
     len = sprintf(buf,"{\"meta\":{\"type\":\"mq.lastCostTime\"},\"data\":{\"hangerCode\":\"800021\",\"lastCostTime\":\"10\",\"hgFinishNumber\":100,\"nextStation\":\"1101-02\",\"planNum\":\"200\",\"faultNumber\":5,\"totalTime\":222,\"totalStdAvgTime\":555}}");
-   // send_frame(COM_ETH,0x80,CMD_READ,len,(unsigned char *)buf);
+    //send_frame(COM_ETH,0x80,CMD_READ,len,(unsigned char *)buf);
     usleep(1000*200);
 
+    len = sprintf(buf,"{\"data\":{\"code\":\"W1004\",\"codeType\":\"E\",\"message\":\"重复出站\"},\"meta\":{\"type\":\"mq.stateCode\"}}");
+
+    send_frame(COM_ETH,0x80,CMD_READ,len,(unsigned char *)buf);
+    usleep(1000*2000);
+
+
+    len = sprintf(buf,"{\"data\":{\"code\":\"W1004\",\"codeType\":\"W\",\"message\":\"重复出站\"},\"meta\":{\"type\":\"mq.stateCode\"}}");
+
+    send_frame(COM_ETH,0x80,CMD_READ,len,(unsigned char *)buf);
+    usleep(1000*200);
 }
 
 void updatetask::parse_file()
